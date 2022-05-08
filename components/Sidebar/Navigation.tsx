340c4f2a12606d14bf47f10ad45baf933ee0320f
DonquixoteSabo <mixboksu@gmail.com>
2022-05-08 15:14:19 +0200

    build: Prepare main architecture

---
+++ components/Sidebar/Navigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NextComponentType } from 'next';
+
+
+const Navigation: NextComponentType = () => {
+  return (
+    <nav>
+      <ul>
+        <li>2022</li>
+        <li>2021</li>
+        <li>2020</li>
+        <li>2019</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navigation from './Navigation';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <h1>Code challenges</h1>
+      <Navigation />
+    </aside>
+  );
+};
+
+export default Sidebar;
